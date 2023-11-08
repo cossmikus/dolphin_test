ec854081df6967341e556c649dd9dd33ac8cd6b1
cossmikus <kassymkhan.bolat@nu.edu.kz>
2023-11-09 01:11:41 +0600

    ffafafaffargjtjf

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,44 +10,43 @@
 //     </main>
 //   )
 // }
-
+// pages/index.tsx
+"use client";
 import { useState } from 'react';
 import Image from 'next/image';
 
-export default function Home() {
-  const [ballY, setBallY] = useState(0);
-  const [isJumping, setIsJumping] = useState(false);
+export default function JumpingGame() {
+  const [jumpHeight, setJumpHeight] = useState(0);
 
   const jump = () => {
-    if (!isJumping) {
-      setIsJumping(true);
-      setBallY(100); // Adjust the jump height as needed
+    if (jumpHeight === 0) {
+      setJumpHeight(100); // Adjust the jump height as needed
       setTimeout(() => {
-        setIsJumping(false);
-        setBallY(0);
+        setJumpHeight(0);
       }, 1000); // Adjust the jump duration as needed
     }
   };
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="relative">
-        <div
-          className="absolute w-16 h-16 bg-red-500 rounded-full"
-          style={{
-            bottom: ballY,
-            transition: isJumping ? 'bottom 1s' : 'none',
-          }}
-        ></div>
-      </div>
-      <div>
-        <button
-          className="bg-blue-500 text-white font-bold py-2 px-4 rounded-full"
-          onClick={jump}
-        >
-          Jump
-        </button>
-      </div>
-    </main>
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <Image
+        src="/download.png" // Replace with your character image
+        alt="Character"
+        width={100}
+        height={100}
+        style={{
+          position: 'relative',
+          bottom: jumpHeight,
+          transition: jumpHeight ? 'bottom 1s' : 'none',
+        }}
+      />
+      <button
+        className="bg-blue-500 text-white font-bold py-2 px-4 rounded-full mt-4"
+        onClick={jump}
+      >
+        Jump
+      </button>
+    </div>
   );
 }
+
